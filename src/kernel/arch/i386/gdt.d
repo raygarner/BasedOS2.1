87b10a45f0cac377e6ab05bc9e245c68ba6fed13
raygarner <ray@perfectcast.co.uk>
2018-12-12 21:39:34 +0000

    clean

--- src/kernel/arch/i386/gdt.d
+++
@@ -1,5 +0,0 @@
-arch/i386/gdt.o: arch/i386/gdt.c include/kernel/gdt.h \
- /home/ray/build-i686-elf/linux/output/lib/gcc/i686-elf/7.1.0/include/stdint.h \
- /home/ray/build-i686-elf/linux/output/lib/gcc/i686-elf/7.1.0/include/stdint-gcc.h \
- /home/ray/programming/BasedOS2.1/src/sysroot/usr/include/stdio.h \
- /home/ray/programming/BasedOS2.1/src/sysroot/usr/include/sys/cdefs.h
